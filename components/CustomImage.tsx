b0b28ca9250632aa5a06bd27e0544d49abe4f817
Nicklas Jarnesjö <nicklas@webready.se>
2021-02-09 21:20:56 +0100

    Removed debug

--- components/CustomImage.tsx
+++ components/CustomImage.tsx
@@ -3,8 +3,6 @@ import Image, {ImageProps} from 'next/image'
 const CustomImage = ({src, ...props}: ImageProps) => {
   const image = require(`../public${src}`)
 
-  console.log(image)
-
   return (
     <div className="relative overflow-hidden flex">
       <img
