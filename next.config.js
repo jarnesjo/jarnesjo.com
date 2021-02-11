de2f48c3060fb2cbaa967159b72b1348e0032a4d
Nicklas Jarnesjö <nicklas@webready.se>
2021-02-11 21:25:29 +0100

    Supress webpack warn for files without loaders

--- next.config.js
+++ next.config.js
@@ -5,34 +5,43 @@ module.exports = {
     ]
   },
   webpack: (config, options) => {
-    config.module.rules.push(
-      {
-        test: /\.(png|jpe?g)$/i,
-        use: [
-          {
-            loader: 'lqip-loader',
-            options: {
-              base64: true,
-              palette: false,
-              publicPath: '/_next',
-              name: 'static/media/[name].[hash].[ext]'
-            }
+    // Blurred image loader
+    config.module.rules.push({
+      test: /\.(png|jpe?g)$/i,
+      use: [
+        {
+          loader: 'lqip-loader',
+          options: {
+            base64: true,
+            palette: false
           }
-        ]
-      },
-      {
-        test: /\.(svg|png|jpe?g|gif|mp4)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              publicPath: '/_next',
-              name: 'static/media/[name].[hash].[ext]'
-            }
+        }
+      ]
+    })
+
+    // Loading images outside of public-directory
+    config.module.rules.push({
+      test: /\.(svg|png|jpe?g|gif|mp4)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next',
+            name: 'static/media/[name].[hash].[ext]'
           }
-        ]
+        }
+      ]
+    })
+
+    // Added only to supress webpack warns about no loader for file type
+    config.module.rules.push({
+      test: /\.(mdx|mp3|ico)$/,
+      use: {
+        loader: 'file-loader',
+        options: {emitFile: false}
       }
-    )
+    })
+
     return config
   }
 }
