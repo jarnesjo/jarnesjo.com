3ab66795298d8e3207a65400f340f5f9ab0dae74
Nicklas Jarnesjö <nicklas@webready.se>
2021-02-18 09:31:47 +0100

    Updated link presentation in prose

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,30 +17,8 @@ module.exports = {
             },
             a: {
               color: theme('colors.gray.700'),
-              backgroundImage: `linear-gradient(0deg,${theme('colors.red.300')} 30%,transparent 0)`,
-              textDecoration: 'none',
               fontWeight: '700',
-              '&:hover': {
-                backgroundImage: `linear-gradient(0deg,${theme('colors.red.300')} 0,${theme(
-                  'colors.red.300'
-                )})`
-              },
-              '&:before': {
-                content: '',
-                display: ' block',
-                height: '.5em',
-                position: ' absolute',
-                left: ' 0',
-                right: ' 0',
-                bottom: ' 0',
-                zIndex: '-1'
-              },
-              '&:after': {
-                backgroundColor: theme('colors.red.700'),
-                transform: 'scaleX(0)',
-                transformOrigin: '0 50%',
-                transition: 'transform .3s cubic-bezier(.86,0,.07,1)'
-              }
+              position: 'relative'
             }
           }
         }
