2e8b2671f1553a065a00ba989fb12f3f32b38f5f
Nicklas Jarnesjö <nicklas@webready.se>
2021-02-26 11:25:02 +0100

    Updated uses

--- pages/uses.tsx
+++ pages/uses.tsx
@@ -1,7 +1,6 @@
 import {CustomImage} from '@/components/CustomImage'
 import {DefaultLayout, defaultMeta} from '@/components/layouts/DefaultLayout'
 import {PageHeading} from '@/components/PageHeading'
-import Head from 'next/head'
 
 export default function UsesPage() {
   return (
@@ -26,10 +25,11 @@ export default function UsesPage() {
 
         <h3>Computer / Office</h3>
         <ul>
-          <li>15" Macbook Pro (2018)</li>
+          <li>15" MacBook Pro (2018)</li>
           <li>37.5" Curved UltraWide LG 38WK95C-W</li>
           <li>Magic Mouse 2</li>
           <li>Magic Keyboard with numeric</li>
+          <li>Twelve South HiRise Notebook stand</li>
           <li>IKEA Bekant with adjustable height</li>
           <li>HÅG Capisco 8107 (saddle chair)</li>
         </ul>
@@ -44,7 +44,7 @@ export default function UsesPage() {
         <h3>Audio</h3>
         <ul>
           <li>Bose NC 700</li>
-          <li>Airpods Pro</li>
+          <li>AirPods Pro</li>
         </ul>
 
         <h3>Other tech</h3>
