98a27d8062bfddc34a7835394ea0bfa927da08b1
Nicklas Jarnesjö <nicklas@webready.se>
2021-09-20 22:35:31 +0200

    Fixed Tailwind purge after restructuring src #nakedcssday

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const {fontFamily} = require('tailwindcss/defaultTheme')
 const darkModeTransitionTime = '200ms'
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/client/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
