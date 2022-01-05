ffd91bcc3ed91deb926b0bfe8f0a2f4f31fbf6af
Nicklas Jarnesjö <nicklas@webready.se>
2022-01-05 14:07:40 +0100

    Added typing

--- src/client/components/PageHeading.tsx
+++ src/client/components/PageHeading.tsx
@@ -1,4 +1,4 @@
-export const PageHeading = ({children}) => {
+export const PageHeading = ({children}: {children: React.ReactChild}) => {
   return (
     <h1 className="text-5xl pt-12 pb-6 md:py-16 font-bold tracking-tight text-center js-darkmode-flicker js-darkmode-tipping">
       {children}
