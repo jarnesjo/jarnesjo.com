259300fd536ce18bda03e0e663ec8b7d81763e7c
Nicklas Jarnesjö <nicklas@webready.se>
2024-04-26 15:19:31 +0200

    Cleaned up

--- src/client/components/CustomImage.tsx
+++ src/client/components/CustomImage.tsx
@@ -5,19 +5,8 @@ export type CustomImageType = ImageProps & {
 }
 
 const CustomImage = ({src, noPlaceholder, ...props}: CustomImageType) => {
-  // const image = src.startsWith('http') ? {src} : require(`../../../public${src}`)
-
   return (
     <div className="relative overflow-hidden flex rounded-none sm:rounded-md">
-      {/* {image.dataURI && !noPlaceholder && (
-        <img
-          src={image.dataURI}
-          className="absolute inset-0 w-full h-full transform scale-110 m-0"
-          style={{filter: 'blur(20px)'}}
-          aria-hidden="true"
-          alt=""
-        />
-      )} */}
       <Image src={src} {...props} />
     </div>
   )
